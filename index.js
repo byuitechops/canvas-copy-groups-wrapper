d8011661e22087952c2d11ef3bf745d5f17c4b60
Seth Childers <sethchilders92@gmail.com>
2019-01-03 18:30:49 -0700

    Working code that is ready to run

---
+++ index.js
@@ -0,0 +1,52 @@
+const copyGroups = require('canvas-copy-groups');
+const d3 = require('d3-dsv');
+const fs = require('fs');
+const path = require('path');
+const canvas = require('canvas-api-wrapper');
+
+function getCsvData() {
+	const file = path.resolve('../Winter2019onlineScaledCoursesGroupReport_1546550796400.csv');
+	const fileData = fs.readFileSync(file, 'utf8');
+	let courses = d3.csvParse(fileData);
+
+	// only get the courses that you want
+	let coursesToRun = courses.filter(course => {
+		if  (course.id !== '39016' &&
+		(course.groupCategories.includes('|Project Groups|') ||
+		course.groupCategories === 'Project Groups|' ||
+		course.groupCategories === 'Grupos de Projeto|')) {
+			return course;
+		}
+	});
+
+	// you should probably sort these alphabetically because 
+	// of how we get the blueprint course id inside of runCourses
+	return coursesToRun;
+}
+
+// these directories must exist to write the log reports
+function makeDirectories() {
+	fs.mkdirSync(path.resolve('./htmlReport'));
+	fs.mkdirSync(path.resolve('./jsonReport'));
+}
+
+async function runCourses() {
+	let parentId;
+	let courseName;
+	let coursesToRun = getCsvData();
+	makeDirectories();
+
+	for (let i = 0; i < coursesToRun.length; i++) {
+		// only do a get request for the blueprint course if you are running a new course
+		// else keep the same blueprint for the various sections of the course
+		if (courseName !== coursesToRun[i].name) {
+			courseName = coursesToRun[i].name;
+			let blueprint = await canvas.get(`/api/v1/courses/${coursesToRun[i].id}/blueprint_subscriptions`);
+			parentId = blueprint[0].blueprint_course.id;
+		}
+		// run the bulk of the code for each section of each course
+		copyGroups(parentId, coursesToRun[i].id, true);
+	}
+}
+
+runCourses();
